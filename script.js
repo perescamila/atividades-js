b9c3132defd9335dbf980cd214450ae460834943
perescamila <126683873+perescamila@users.noreply.github.com>
2024-04-03 11:39:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+//01)Declare duas variáveis e atribua a elas valores numéricos. Em seguida, exiba a soma desses números no console.
+
+let x=50;
+let y=20;
+let soma;
+
+soma= x+y;
+
+console.log(soma);
+
+//02)Declare duas variáveis e atribua a elas valores numéricos. Em seguida, exiba a subtração desses números no console
+let a =60;
+let b=10;
+let subtração;
+
+subtração=a-b;
+
+console.log(subtração);
+
+//03)Declare duas variáveis e atribua a elas valores numéricos. Em seguida, exiba o produto desses números no console
+let c =70;
+let d=10;
+let produto;
+
+produto=c*d;
+
+console.log(produto);
+
+//04)Declare duas variáveis e atribua a elas valores numéricos. Em seguida, exiba a divisão desses números no console.
+let e =80;
+let f =90;
+let divisão;
+
+divisão=e/f;
+
+console.log(divisão);
+
+//05)Declare uma variável e atribua a ela um valor numérico. Em seguida, exiba o quadrado desse número no console
+let g=400;
+let h= 4;
+let potencia;
+
+potencia=400^4;
+
+console.log(potencia);
+
+//06)Declare uma variável e atribua a ela um valor numérico. Em seguida, exiba a raiz quadrada desse número no console.
+let i=20;
+let j=8;
+let raiz;
+
+raiz =Math.sqrt(606)
+
+console.log (raiz)
+
+//07)Declare uma variável e atribua a ela um valor numérico. Em seguida, exiba o módulo desse número (valor absoluto) no console
+let k=30;
+let l=60;
+let absoluto
+
+absoluto=30;
+
+console.log(absoluto)
+
+//08)Declare uma variável e atribua a ela um valor numérico. Em seguida, exiba se o número é par ou ímpar no console.
+let m=70;
+let n=3;
+let ímpar
+
+
+
